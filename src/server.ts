ca4f60f0ee04aa77a68d1173b93e3ec73df12bef
David Jiang <integralhero@gmail.com>
2020-04-07 07:19:14 -0700

    Add filtered image endpoint

--- src/server.ts
+++ src/server.ts
@@ -27,6 +27,14 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+app.get('/filteredimage', async(req, res) => {
+  const { image_url } = req.query;
+  if(!image_url) { res.status(400).send('missing image_url query param') };
+  const filteredpath = await filterImageFromURL(image_url);
+  res.on('finish', () => { deleteLocalFiles([filteredpath]); });
+  return res.sendFile(filteredpath);
+});
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
